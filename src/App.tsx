6bfad6abee3a6f21a924fa513e902251b20384f3
daisukeokamoto1023 <okadaih11023@gmail.com>
2021-05-03 10:27:43 +0900

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,42 @@
-import React, { useState } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-import Subsidies from "./components/SubsidyList"
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>補助金のまとめ</p>
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Subsidies from "./components/SubsidyList";
+import StickyFooter from "./components/StickyFooter";
 
-                <p>
-                    <Subsidies />
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    {" | "}
-                    <a
-                        className="App-link"
-                        href="https://vitejs.dev/guide/features.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Vite Docs
-                    </a>
-                </p>
-            </header>
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>補助金のまとめ</p>
 
-export default App
+        <p>
+          <Subsidies />
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+      <StickyFooter />
+    </div>
+  );
+};
+
+export default App;
