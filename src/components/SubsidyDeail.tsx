391749c621e51ddcd27c09d37e1d8ac8a3a25bfa
daisukeokamoto1023 <okadaih11023@gmail.com>
2021-05-08 11:08:18 +0900

    draw line around cards

--- src/components/SubsidyDeail.tsx
+++ src/components/SubsidyDeail.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import {createStyles, Theme, makeStyles} from "@material-ui/core/styles";
 import {Box, Container, Typography, Paper} from "@material-ui/core";
 import {Card, CardContent, CardHeader, CardActions} from "@material-ui/core";
-import List from "@material-ui/core/List";
-import ListItem, {ListItemProps} from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
+import ReactMarkdown from "react-markdown";
 type SubsidyDetailProps = {
   values: subsidy.TargetContent;
 };
@@ -15,6 +12,12 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "100%",
       maxWidth: 1080,
       backgroundColor: theme.palette.background.paper,
+    },
+    card: {
+      border: "2px solid",
+      borderColor: "#E7EDF3",
+      borderRadius: 16,
+      transition: "0.4s",
     },
   })
 );
@@ -34,7 +37,7 @@ const SubsidyDetail: React.FC<SubsidyDetailProps> = (
         <Typography variant="h6" color="textSecondary" component="p">
           制度番号：{props.values.number}
         </Typography>
-        <Card className={classes.root}>
+        <Card className={classes.card}>
           <CardHeader title="対象者" align="left" />
           <CardContent>
             <Typography variant="body1" color="textSecondary" component="p">
@@ -42,16 +45,16 @@ const SubsidyDetail: React.FC<SubsidyDetailProps> = (
             </Typography>
           </CardContent>
         </Card>
-        <Card className={classes.root}>
+        <Card className={classes.card}>
           <CardHeader title="内容" align="left" />
           <CardContent>
             <Typography variant="body1" color="textSecondary" component="p">
-              {props.values.body}
+              <ReactMarkdown children={props.values.body} skipHtml={true} />
             </Typography>
           </CardContent>
         </Card>
 
-        <Card className={classes.root}>
+        <Card className={classes.card}>
           <CardHeader title="利用・申請方法" align="left" />
           <CardContent>
             <Typography variant="body1" color="textSecondary" component="p">
@@ -59,7 +62,7 @@ const SubsidyDetail: React.FC<SubsidyDetailProps> = (
             </Typography>
           </CardContent>
         </Card>
-        <Card className={classes.root}>
+        <Card className={classes.card}>
           <CardHeader title="お問い合わせ先" align="left" />
           <CardContent>
             <Typography variant="body1" color="textSecondary" component="p">
